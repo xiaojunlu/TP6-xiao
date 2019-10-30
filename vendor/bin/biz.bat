3c0b37d90a991f552530226c121af728c34be9f1
luxiaojun <960972305@qq.com>
2019-10-30 16:33:49 +0800

    88888

--- vendor/bin/biz.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../codeages/biz-framework/bin/biz
-php "%BIN_TARGET%" %*
--- vendor/bin/phpmig.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../davedevelopment/phpmig/bin/phpmig
-php "%BIN_TARGET%" %*
